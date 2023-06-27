a5b48443b4ca9a6537a40653f7acc664bebbc2aa
Nata <rybapoint@gmail.com>
2023-06-27 11:49:35 +0300

    test.js filter task from cadewar uber2_project

--- js/test.js
+++ js/test.js
@@ -1,1 +1,10 @@
-console.log('test');
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  var arr = [];
+  console.log(l);
+  
+  arr = l.filter(elem => Number.isInteger(elem));
+  return arr;
+}
+var fff =  filter_list([1,2,'a','b']);
+console.log(fff);
