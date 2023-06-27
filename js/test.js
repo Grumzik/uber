d2004a59fe99c23c08285a4018215906640daebb
Nata <rybapoint@gmail.com>
2023-06-27 12:30:53 +0300

    need merge 2 - gloves-task from codeware in test.js

--- js/test.js
+++ js/test.js
@@ -8,3 +8,19 @@ function filter_list(l) {
 }
 var fff =  filter_list([1,2,'a','b']);
 
+function numberOfPairs(gloves)
+{
+  var pair = 0;
+
+  gloves.sort();
+  
+  for (var i=0; i<(gloves.length -1); i++){
+    if (gloves[i] === gloves[i+1]) {
+      pair++;
+      i++;
+    }
+  }
+  return pair;
+}
+
+numberOfPairs(['gray','black','purple','purple','gray','black']);
